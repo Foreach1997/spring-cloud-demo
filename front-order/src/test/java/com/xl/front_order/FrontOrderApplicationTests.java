ac79d1f827d4edf2500b4e80ef13e9fa6a0443fb
ljw <ljw>
2020-04-20 11:22:38 +0800

    Initial commit

---
+++ front-order/src/test/java/com/xl/front_order/FrontOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xl.front_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
