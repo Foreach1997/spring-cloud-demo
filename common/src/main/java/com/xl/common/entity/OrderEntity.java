cab966dd65628749b90a561ae456f9375a6eb393
ljw <ljw>
2020-04-24 12:22:17 +0800

    提交实体

---
+++ common/src/main/java/com/xl/common/entity/OrderEntity.java
@@ -0,0 +1,45 @@
+package com.xl.common.entity;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "order", schema = "cloud", catalog = "")
+public class OrderEntity {
+    private long id;
+    private String orderNo;
+
+    @Id
+    @Column(name = "id")
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "order_no")
+    public String getOrderNo() {
+        return orderNo;
+    }
+
+    public void setOrderNo(String orderNo) {
+        this.orderNo = orderNo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderEntity that = (OrderEntity) o;
+        return id == that.id &&
+                Objects.equals(orderNo, that.orderNo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, orderNo);
+    }
+}
